6c4aa2ab6b7864070179d57f126ee108a159b481
aamir1995 <syedaamir974@gmail.com>
2022-06-15 16:52:38 +0500

    data-rest-api: add spinner as fallback while loading

--- src/pages/data-rest-api/index.tsx
+++ src/pages/data-rest-api/index.tsx
@@ -1,17 +1,39 @@
 import React, { useEffect } from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import Layout from '@theme/Layout';
+import Spinner from '@site/src/components/misc/spinner';
 
 export default function DataRestAPI(): JSX.Element {
 
   useEffect(() => {
     const ele: HTMLDivElement = document.querySelector('div.main-wrapper');
-    ele.classList.add('data-rest-api-wrapper');
+
+    let observer: MutationObserver;
+
+    if (ele) {
+      ele.classList.add('data-rest-api-wrapper');
+    } else {
+      observer = new MutationObserver(() => { // wait for the wrapper to be available using MutationObserver
+        const ele: HTMLDivElement = document.querySelector('div.main-wrapper');
+        if (ele) {
+          observer.disconnect();
+          observer = null;
+          ele.classList.add('data-rest-api-wrapper');
+        }
+      });
+      observer.observe(document, { subtree: true, childList: true });
+    }
+    return () => {
+      if (observer) {
+        observer.disconnect();
+        observer = null;
+      }
+    }
   }, [])
 
   return (
     <BrowserOnly
-      fallback={<h1>loading</h1>}
+      fallback={<Spinner wrapperStyles={{ width: 32, height: 32, display: 'flex', justifyContent: 'center', alignItems: 'center', alignSelf: 'center' }} />}
     >
       {() => {
         const { API } = require('@stoplight/elements');
