179bed81688b8348ef3118911c07c7670d43e5f2
aamir1995 <syedaamir974@gmail.com>
2022-06-30 20:42:05 +0500

    initial commit: update design of homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,25 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Waves from '@site/static/img/waves.svg';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <header className={clsx('hero', styles.headerBanner)}>
+      <div className={clsx(styles.headerContainer, 'container')}>
+        <div style={{}}></div>
+        <h1 className={clsx("hero__title", styles.headerTagLine)}>The world´s<br />most powerful<br />3D viewer</h1>
+        {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className={styles.headerButtonMain}
             to="/docs/intro">
             Get Started
           </Link>
         </div>
       </div>
+      <Waves style={{ position: 'absolute', bottom: 0, width: '100%' }} />
     </header>
   );
 }
