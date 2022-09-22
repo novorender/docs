c01ee77a2d7d16cc32a6888d4dec5cdd1bcb9edc
aamir1995 <syedaamir974@gmail.com>
2022-09-22 16:43:23 +0500

    fix: demo crash

--- src/components/PlaygroundComponent/index.tsx
+++ src/components/PlaygroundComponent/index.tsx
@@ -12,7 +12,7 @@ export const predefined_scenes = ['cube', 'condos', 'oilrig', 'empty'] as const;
 export interface PlaygroundConfig {
     mode: 'inline' | 'fill',
     clickToRun?: boolean,
-    useManagedRenderer?: boolean
+    useManagedRenderer?: boolean;
 };
 
 interface props {
@@ -21,7 +21,7 @@ interface props {
     demoName: string,
     editUrl?: string,
     config?: PlaygroundConfig,
-    cameraController?: CameraControllerParams
+    cameraController?: CameraControllerParams;
 };
 
 export default function PlaygroundComponent({ children, scene, demoName, cameraController = { kind: 'static' }, config, editUrl }: props): JSX.Element {
@@ -56,17 +56,22 @@ export default function PlaygroundComponent({ children, scene, demoName, cameraC
     return (
         <BrowserOnly fallback={<div>Loading...</div>}>
             {
-                () => <div style={playgroundConfig.mode === 'inline' ? { width: 769, border: '2px solid #d5275d33', padding: 5 } : { height: 'calc(100vh - 60px)', overflow: 'hidden', paddingTop: 2 }}>
-                    {playgroundConfig.clickToRun && (!isPlaygroundActive || (isPlaygroundActive && demoName !== runningPlaygroundId)) ?
-                        <div style={{ position: 'relative' }} onMouseEnter={() => { toggleTip(true) }} onMouseLeave={() => { toggleTip(false) }}>
-                            <CSSTransition in={showTip} timeout={300} classNames={'alert'} unmountOnExit>
-                                <button onClick={runPlayground} className="button button--lg button--success" style={{ position: 'absolute', zIndex: 99, right: 0, left: 0, margin: 'auto', top: 0, width: 300, bottom: 0, height: 60 }}>Run this demo</button>
-                            </CSSTransition>
-                            <img src={require(`@site/static/assets/demo-screenshots/${demoName}.jpg`).default} style={{ width: '100%', height: '100%', display: 'block', filter: showTip ? 'brightness(0.4)' : '' }} />
-                        </div>
-                        : <>{playgroundConfig && <MonacoWrapper scene={scene} demoName={demoName} playgroundConfig={playgroundConfig} cameraController={cameraController} editUrl={editUrl}>{children}</MonacoWrapper>}</>}
-                </div>
+                () =>
+                    <div>
+                        {playgroundConfig &&
+                            <div style={playgroundConfig.mode === 'inline' ? { width: 769, border: '2px solid #d5275d33', padding: 5 } : { height: 'calc(100vh - 60px)', overflow: 'hidden', paddingTop: 2 }}>
+                                {playgroundConfig.clickToRun && (!isPlaygroundActive || (isPlaygroundActive && demoName !== runningPlaygroundId)) ?
+                                    <div style={{ position: 'relative' }} onMouseEnter={() => { toggleTip(true); }} onMouseLeave={() => { toggleTip(false); }}>
+                                        <CSSTransition in={showTip} timeout={300} classNames={'alert'} unmountOnExit>
+                                            <button onClick={runPlayground} className="button button--lg button--success" style={{ position: 'absolute', zIndex: 99, right: 0, left: 0, margin: 'auto', top: 0, width: 300, bottom: 0, height: 60 }}>Run this demo</button>
+                                        </CSSTransition>
+                                        <img src={require(`@site/static/assets/demo-screenshots/${demoName}.jpg`).default} style={{ width: '100%', height: '100%', display: 'block', filter: showTip ? 'brightness(0.4)' : '' }} />
+                                    </div>
+                                    : <>{playgroundConfig && <MonacoWrapper scene={scene} demoName={demoName} playgroundConfig={playgroundConfig} cameraController={cameraController} editUrl={editUrl}>{children}</MonacoWrapper>}</>}
+                            </div>
+                        }
+                    </div>
             }
         </BrowserOnly>
-    )
+    );
 }
