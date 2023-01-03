dabf0c020be9af0fdb1ca3964e3c67b571b406a7
aamir1995 <syedaamir974@gmail.com>
2023-01-03 17:55:53 +0500

    2d canvas for drawing2d tut

--- src/components/Renderer/index.tsx
+++ src/components/Renderer/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import type { API, RenderSettingsParams, View, EnvironmentDescription, CameraControllerParams } from "@novorender/webgl-api";
 import type { MeasureAPI } from "@novorender/measure-api";
+import * as glMatrix from 'gl-matrix';
 interface props {
     main: any;
     isDoingActivity: (a: boolean) => void;
@@ -75,6 +76,7 @@ interface props {
 export default function Renderer({ main, isDoingActivity, canvasRef, api, measureApiInstance, panesHeight, panesWidth, onMessagesAndAlert }: props): JSX.Element {
 
     const canvas = useRef<HTMLCanvasElement>(null);
+    const canvas2D = useRef<HTMLCanvasElement>(null);
     // const [view, setView] = useState<View>(null);
     const [apiInstance, setApiInstance] = useState<API>(api.createAPI()); // Create API
     const [_measureApiInstance, setMeasureApiInstance] = useState<MeasureAPI>(measureApiInstance.createMeasureAPI()); // Create API
@@ -156,6 +158,10 @@ export default function Renderer({ main, isDoingActivity, canvasRef, api, measur
                 switch (main.length) {
                     case 3:
                         await main(apiInstance, canvas.current, measureApiInstance);
+                        break;
+
+                    case 5:
+                        await main(apiInstance, canvas.current, measureApiInstance, glMatrix, canvas2D.current);
                         break;
 
                     default:
@@ -293,8 +299,9 @@ export default function Renderer({ main, isDoingActivity, canvasRef, api, measur
     return (
         <BrowserOnly>
             {
-                () => <div style={{ height: panesHeight }} className="canvas-overscroll-fix">
+                () => <div style={{ height: panesHeight, position: 'relative' }} className="canvas-overscroll-fix">
                     <canvas ref={canvas} style={{ width: '100%', height: '100%' }}></canvas>
+                    <canvas ref={canvas2D} style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 }}></canvas>
                 </div>
             }
         </BrowserOnly>
