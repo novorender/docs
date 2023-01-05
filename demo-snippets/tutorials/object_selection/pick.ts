51d971357878527aee8002d917384c0404c465e4
Ola Folkvord <ola.folki@gmail.com>
2023-01-05 14:56:55 +0100

    fix object selection example

--- demo-snippets/tutorials/object_selection/pick.ts
+++ demo-snippets/tutorials/object_selection/pick.ts
@@ -1,66 +1,100 @@
-import * as NovoRender from "@novorender/webgl-api";
-import * as MeasureAPI from '@novorender/measure-api';
-import * as DataJsAPI from '@novorender/data-js-api';
-import * as GlMatrix from 'gl-matrix';
+import * as Novorender from "@novorender/webgl-api";
+import * as MeasureAPI from "@novorender/measure-api";
+import * as DataJsAPI from "@novorender/data-js-api";
+import * as GlMatrix from "gl-matrix";
 export interface IParams {
-  webglAPI: NovoRender.API;
+  webglAPI: Novorender.API;
   canvas: HTMLCanvasElement;
   measureAPI: typeof MeasureAPI;
   dataJsAPI: typeof DataJsAPI;
   glMatrix: typeof GlMatrix;
   canvas2D: HTMLCanvasElement;
-};
+}
 
 export async function main({ webglAPI, canvas }: IParams) {
-    // create a view
-    const view = await webglAPI.createView({ background: { color: [0, 0, 0.1, 1] } }, canvas);
+  // Init
+  const view = await initView(webglAPI, canvas);
+  const scene = view.scene!;
+  run(view, canvas);
 
-    // provide a camera controller
-    view.camera.controller = webglAPI.createCameraController({ kind: "flight" }, canvas);
+  // Set up highlight groups
+  const desaturated = webglAPI.createHighlight({
+    kind: "hsla",
+    saturation: 0.5,
+  });
+  const limeGreen = webglAPI.createHighlight({
+    kind: "color",
+    color: [0, 1, 0],
+  });
 
-    // create an empty scene
-    const scene = view.scene = await webglAPI.loadScene(NovoRender.WellKnownSceneUrls.condos);
+  view.settings.objectHighlights = [desaturated, limeGreen];
 
-    // create a bitmap context to display render output
-    const ctx = canvas.getContext("bitmaprenderer");
+  // Listen to click events to pick objects
+  canvas.addEventListener("click", async (e) => {
+    const result = await view.lastRenderOutput?.pick(e.offsetX, e.offsetY);
+    if (result) {
+      // Reset highlights
+      // Here we set all objects to use the highlight found at view.settings.objectHighlights[0]
+      // In this case "desaturated"
+      scene.objectHighlighter.objectHighlightIndices.fill(0);
 
-    // make object highlights
-    const highlightGroup0 = webglAPI.createHighlight({ kind: "hsla", saturation: 0.5 });
-    const highlightGroup1 = webglAPI.createHighlight({ kind: "color", color: [0, 1, 0] });
-    view.settings.objectHighlights = [highlightGroup0, highlightGroup1];
+      // Set selected object to use highlight found at view.settings.objectHighlights[1]
+      // In this case "limeGreen"
+      scene.objectHighlighter.objectHighlightIndices[result.objectId] = 1;
 
-    // Make a simple state object that we can share between mouse events and render loop
-    const state = { output: undefined as NovoRender.RenderOutput | undefined };
-
-    // Add mouse click event handler to pick objects
-    canvas.addEventListener("click", async (e) => {
-        await view.updatePickBuffers();
-        const result = await state.output?.pick(e.offsetX, e.offsetY);
-        if (result) {
-            const { objectId } = result;
-            scene.objectHighlighter.objectHighlightIndices[objectId] = 1;
-            scene.objectHighlighter.commit();
-        }
-    });
-
-    // main render loop
-    for (; ;) {
-        // handle canvas resizes
-        const { clientWidth, clientHeight } = canvas;
-        const width = Math.round(clientWidth);
-        const height = Math.round(clientHeight);
-        view.applySettings({ display: { width, height } });
-
-        // render frame
-        state.output = await view.render();
-        {
-            // finalize output image
-            const image = await state.output.getImage();
-            if (image) {
-                // display in canvas
-                ctx?.transferFromImageBitmap(image);
-            }
-            image?.close();
-        }
+      scene.objectHighlighter.commit();
     }
+  });
+}
+
+async function initView(webglApi: Novorender.API, canvas: HTMLCanvasElement) {
+  // Create a view
+  const view = await webglApi.createView(
+    { background: { color: [0, 0, 0, 0] } }, // Transparent
+    canvas
+  );
+
+  // Provide a camera controller
+  view.camera.controller = webglApi.createCameraController(
+    { kind: "flight" },
+    canvas
+  );
+
+  // Load the Condos demo scene
+  view.scene = await webglApi.loadScene(Novorender.WellKnownSceneUrls.condos);
+
+  return view;
+}
+
+async function run(view: Novorender.View, canvas: HTMLCanvasElement) {
+  // Create a bitmap context to display render output
+  const ctx = canvas.getContext("bitmaprenderer");
+
+  // Handle canvas resizes
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      canvas.width = entry.contentRect.width;
+      canvas.height = entry.contentRect.height;
+      view.applySettings({
+        display: { width: canvas.width, height: canvas.height },
+      });
+    }
+  });
+
+  resizeObserver.observe(canvas);
+
+  // Main render loop
+  while (true) {
+    // Render frame
+    const output = await view.render();
+    {
+      // Finalize output image
+      const image = await output.getImage();
+      if (image) {
+        // Display in canvas
+        ctx?.transferFromImageBitmap(image);
+        image.close();
+      }
+    }
+  }
 }
