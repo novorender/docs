e28b557e78be5f4f8c89bb1a262ff170d7106b6f
aamir1995 <syedaamir974@gmail.com>
2023-01-19 18:04:57 +0500

    fix: minor fix

--- demo-snippets/misc.ts
+++ demo-snippets/misc.ts
@@ -1,11 +1,13 @@
 export type IDemo = Record<string, {
-    /** A friendly name for the demo */
+    /** A friendly name for the demo (must be unique) */
     demoName: string,
     config: {
         /** should playground start automatically? defaults to `true` */
         clickToRun?: boolean;
         /** whether to enable a secondary canvas for 2D or not, defaults to `false` */
         canvas2D?: boolean;
+        /** using `fill` will make the playground to take entire viewport's width and height, `inline` is default */
+        mode?: 'inline' | 'fill';
     },
     /** Github edit URL, should be relative path e.g. `demo-snippets/tutorials/clipping_volumes/box.ts` */
     editUrl: string,
