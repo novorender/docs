b7919e03fa015cd56f75cc4372438dc5f6595944
aamir1995 <syedaamir974@gmail.com>
2023-01-25 15:05:16 +0500

    fix(playground): revert unintentional changes.

--- src/components/MonacoWrapper/index.tsx
+++ src/components/MonacoWrapper/index.tsx
@@ -261,19 +261,19 @@ export default function MonacoWrapper({ code, renderSettings, scene, demoName, c
             // });
 
             // compiler options
-            console.log('comppp ', monaco.languages.typescript.typescriptDefaults.getCompilerOptions());
-            console.log('ext12 ', monaco.languages.typescript.typescriptDefaults.getExtraLibs());
+            // console.log('comppp ', monaco.languages.typescript.typescriptDefaults.getCompilerOptions());
+            // console.log('ext12 ', monaco.languages.typescript.typescriptDefaults.getExtraLibs());
 
-              monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-                // moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-                // allowSyntheticDefaultImports: true,
-                moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-                module: monaco.languages.typescript.ModuleKind.ESNext,
-                allowNonTsExtensions: true,
-                // baseUrl: '.',
-                // paths: { "@/*": ['node_modules/*'] },
-                target: monaco.languages.typescript.ScriptTarget.Latest
-              });
+            //   monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+            //     // moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+            //     // allowSyntheticDefaultImports: true,
+            //     moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+            //     module: monaco.languages.typescript.ModuleKind.ESNext,
+            //     allowNonTsExtensions: true,
+            //     // baseUrl: '.',
+            //     // paths: { "@/*": ['node_modules/*'] },
+            //     target: monaco.languages.typescript.ScriptTarget.Latest
+            //   });
 
             const libUri = "index.d.ts";
             monaco.languages.typescript.typescriptDefaults.addExtraLib(
