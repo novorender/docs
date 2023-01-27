f1f408675076c8434802c72176f33046e2a744bd
aamir1995 <syedaamir974@gmail.com>
2023-01-27 16:48:37 +0500

    feat(playground): added cube demo, some minor fixes

---
+++ demo-snippets/tutorials/getting_started/basic_cube.ts
@@ -0,0 +1,43 @@
+import * as NovoRender from "@novorender/webgl-api";
+import * as MeasureAPI from '@novorender/measure-api';
+import * as DataJsAPI from '@novorender/data-js-api';
+import * as GlMatrix from 'gl-matrix';
+
+export interface IParams {
+    webglAPI: NovoRender.API;
+    canvas: HTMLCanvasElement;
+    measureAPI: typeof MeasureAPI;
+    dataJsAPI: typeof DataJsAPI;
+    glMatrix: typeof GlMatrix;
+    canvas2D: HTMLCanvasElement;
+};
+
+export async function main({ webglAPI, canvas }: IParams) {
+
+    // Create a view
+    const view = await webglAPI.createView({ background: { color: [0, 0, 0.25, 1] } }, canvas);
+
+    // load a predefined scene into the view, available views are cube, oilrig, condos
+    view.scene = await webglAPI.loadScene(NovoRender.WellKnownSceneUrls.cube);
+
+    // provide a controller, available controller types are static, orbit, flight and turntable
+    view.camera.controller = webglAPI.createCameraController({ kind: "turntable" });
+
+    const ctx = canvas.getContext("bitmaprenderer");
+    for (; ;) { // render-loop https://dens.website/tutorials/webgl/render-loop
+
+        const { clientWidth: width, clientHeight: height } = canvas;
+        // handle resizes
+        view.applySettings({ display: { width, height } });
+        const output = await view.render();
+
+        {
+            const image = await output.getImage();
+            if (image) {
+                // display in canvas
+                ctx?.transferFromImageBitmap(image);
+            }
+        }
+        (output as any).dispose();
+    }
+}
--- demo-snippets/tutorials/getting_started/index.ts
+++ demo-snippets/tutorials/getting_started/index.ts
@@ -1,7 +1,11 @@
-import demoScene from '!!./demo_scene.ts?raw';
+import basicCube from '!!./basic_cube.ts?raw';
+import basicCondos from '!!./basic_condos.ts?raw';
+import basicCondos1 from '!!./basic_condos_1.ts?raw';
+
 import { demo } from "../../misc";
 
 export const gettingStarted = {
-    ...demo("demoScene1", demoScene, {}, 'A basic demonstration of the WebGL API without `ResizeObserver`.'),
-    ...demo("demoScene", demoScene, {}, 'A basic demonstration of the WebGL API.'),
+    ...demo("basicCube", basicCube, {}, 'A basic demonstration of the WebGL API using a predefined `cube` scene.'),
+    ...demo("basicCondos", basicCondos, {}, 'A basic demonstration of the WebGL API.'),
+    ...demo("basicCondos1", basicCondos1, {}, 'A basic demonstration of the WebGL API using `ResizeObserver`.'),
 };
