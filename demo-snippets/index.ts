f890ddc449da4befe0f01b668f7860dacaea2b49
Aamir Shah <syedaamir974@gmail.com>
2023-02-02 16:25:07 +0500

    chore(playground): remove redundant managed-renderer. (#10)

--- demo-snippets/index.ts
+++ demo-snippets/index.ts
@@ -1,11 +1,3 @@
-/**
- * must export these configs to generate screenshots for the demos.
- */
-
-import * as simpleCubeConfig from './simple-cube';
-import * as anotherTestCubeConfig from './another-test-cube';
 import * as tutorials from './tutorials';
-// export * as clippingVolumes from "./tutorials/clipping_volumes";
 
-
-export { simpleCubeConfig, anotherTestCubeConfig, tutorials }
+export { tutorials }
