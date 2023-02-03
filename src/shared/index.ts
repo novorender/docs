c87a2911df991ab76c9b4fb2117cc643775ca04b
aamir1995 <syedaamir974@gmail.com>
2023-02-03 18:21:37 +0500

    chore: cleanup

--- src/shared/index.ts
+++
@@ -1,9 +0,0 @@
-/**
-    * List of well known scene ids used for testing or demos.
-    */
-export enum WellKnownSceneUrls {
-    empty = "empty",
-    cube = "cube",
-    oilrig = "https://blobs.novorender.com/9fb7bdf8ae8445189573681194718db8/",
-    condos = "https://blobs.novorender.com/1169297611ae33f63132f264ed34e265/",
-}
