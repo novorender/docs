de9bf0a0d82094f52755859e6f6ae13073c87bb9
aamir1995 <syedaamir974@gmail.com>
2023-02-09 20:07:36 +0500

    feat(demo-snippets): divide search demo snippets into multiple individual snippets.

---
+++ demo-snippets/tutorials/searching/exact_search_excluding_result.ts
@@ -0,0 +1,134 @@
+import * as NovoRender from "@novorender/webgl-api";
+import * as MeasureAPI from "@novorender/measure-api";
+import * as DataJsAPI from "@novorender/data-js-api";
+import * as GlMatrix from "gl-matrix";
+
+export interface IParams {
+  webglAPI: NovoRender.API;
+  canvas: HTMLCanvasElement;
+  measureAPI: typeof MeasureAPI;
+  dataJsAPI: typeof DataJsAPI;
+  glMatrix: typeof GlMatrix;
+  canvas2D: HTMLCanvasElement;
+}
+
+export async function main({ webglAPI, canvas, dataJsAPI }: IParams) {
+  try {
+    // Init
+    const view = await initView(webglAPI, canvas, dataJsAPI);
+    run(view, canvas);
+
+    const scene = view.scene!;
+
+    // Run the searches
+    // Same as the exact search, but with exclude. This will return all object except the ones found above.
+    const iterator = scene.search({
+      searchPattern: [
+        { property: "ifcClass", value: "ifcRoof", exact: true, exclude: true },
+      ],
+    });
+
+    // In this example we just want to isolate the objects so all we need is the object ID
+    const result: number[] = [];
+    for await (const object of iterator) {
+      result.push(object.id);
+    }
+
+    // Then we isolate the objects found
+    isolateObjects(scene, result);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+function isolateObjects(scene: NovoRender.Scene, ids: number[]): void {
+  // Set highlight 255 on all objects
+  // Highlight index 255 is reserved fully transparent
+  scene.objectHighlighter.objectHighlightIndices.fill(255);
+
+  // Set highlight back to 0 for objects to be isolated
+  // Highlight 0 should be neutral as we haven't changed view.settings.objectHighlights
+  ids.forEach((id) => (scene.objectHighlighter.objectHighlightIndices[id] = 0));
+
+  scene.objectHighlighter.commit();
+}
+
+async function initView(
+  api: NovoRender.API,
+  canvas: HTMLCanvasElement,
+  dataJsAPI: typeof DataJsAPI
+): Promise<NovoRender.View> {
+  // Initialize the data API with the Novorender data server service
+  const dataApi = dataJsAPI.createAPI({
+    serviceUrl: "https://data.novorender.com/api",
+  });
+
+  // Load scene metadata
+  const sceneData = await dataApi
+    // Condos scene ID, but can be changed to any public scene ID
+    .loadScene("3b5e65560dc4422da5c7c3f827b6a77c")
+    .then((res) => {
+      if ("error" in res) {
+        throw res;
+      } else {
+        return res;
+      }
+    });
+
+  // Destructure relevant properties into variables
+  const { url, db, settings, camera: cameraParams } = sceneData;
+
+  // Load scene
+  const scene = await api.loadScene(url, db);
+
+  // Create a view with the scene's saved settings
+  const view = await api.createView(settings, canvas);
+
+  // Set resolution scale to 1
+  view.applySettings({ quality: { resolution: { value: 1 } } });
+
+  // Create a camera controller with the saved parameters with turntable as fallback
+  const camera = cameraParams ?? ({ kind: "turntable" } as any);
+  view.camera.controller = api.createCameraController(camera, canvas);
+
+  // Assign the scene to the view
+  view.scene = scene;
+
+  return view;
+}
+
+async function run(
+  view: NovoRender.View,
+  canvas: HTMLCanvasElement
+): Promise<void> {
+  // Handle canvas resizes
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      canvas.width = entry.contentRect.width;
+      canvas.height = entry.contentRect.height;
+      view.applySettings({
+        display: { width: canvas.width, height: canvas.height },
+      });
+    }
+  });
+
+  resizeObserver.observe(canvas);
+
+  // Create a bitmap context to display render output
+  const ctx = canvas.getContext("bitmaprenderer");
+
+  // Main render loop
+  while (true) {
+    // Render frame
+    const output = await view.render();
+    {
+      // Finalize output image
+      const image = await output.getImage();
+      if (image) {
+        // Display in canvas
+        ctx?.transferFromImageBitmap(image);
+        image.close();
+      }
+    }
+  }
+}
--- demo-snippets/tutorials/searching/index.ts
+++ demo-snippets/tutorials/searching/index.ts
@@ -1,6 +1,13 @@
-import searchingDemo from '!!./searching.ts?raw';
+import searchByPath from '!!./search_by_path.ts?raw';
+import fluffySearch from '!!./fluffy_search.ts?raw';
+import exactSearch from '!!./exact_search.ts?raw';
+import exactSearchExcludingResult from '!!./exact_search_excluding_result.ts?raw';
+
 import { demo } from "../../misc";
 
 export const searching = {
-    ...demo("searching", "searching", searchingDemo, {}, 'Demonstration a few different search patterns where the resulting objects are isolated in the view.'),
+    ...demo("searching", "searchByPath", searchByPath, { hiddenAreas: [{ startLineNumber: 0, endLineNumber: 13 }, { startLineNumber: 57, endLineNumber: 136 }] }, 'Demonstration path search pattern where the resulting objects are isolated in the view.'),
+    ...demo("searching", "fluffySearch", fluffySearch, { hiddenAreas: [{ startLineNumber: 0, endLineNumber: 13 }, { startLineNumber: 53, endLineNumber: 132 }] }, 'Fluffy search pattern which will search all properties for words starting with "Roof".'),
+    ...demo("searching", "exactSearch", exactSearch, { hiddenAreas: [{ startLineNumber: 0, endLineNumber: 13 }, { startLineNumber: 54, endLineNumber: 133 }] }, 'Exact search only checking the property "ifcClass" and the exact value "ifcRoof".'),
+    ...demo("searching", "exactSearchExcludingResult", exactSearchExcludingResult, { hiddenAreas: [{ startLineNumber: 0, endLineNumber: 13 }, { startLineNumber: 56, endLineNumber: 135 }] }, 'Same as the Exact search pattern, but with exclude. This will return all objects except the ones found above.'),
 };
