e12953c5860c12e5ef04e8febafbf3ecef39f142
aamir1995 <syedaamir974@gmail.com>
2023-02-24 19:06:12 +0500

    fix(snippets - drawing_2d): attempt to fix drawing issue on arm macs

--- demo-snippets/tutorials/drawing2D/drawing_2d.ts
+++ demo-snippets/tutorials/drawing2D/drawing_2d.ts
@@ -458,9 +458,9 @@ export async function main({ webglAPI, canvas, glMatrix, canvas2D, measureAPI }:
     for (; ;) {
         // handle canvas resizes
         const { clientWidth, clientHeight } = canvas;
-        const width = Math.round(clientWidth * devicePixelRatio);
-        const height = Math.round(clientHeight * devicePixelRatio);
-        view.applySettings({ display: { width, height } });
+        // const width = Math.round(clientWidth * devicePixelRatio);
+        // const height = Math.round(clientHeight * devicePixelRatio);
+        view.applySettings({ display: { width: clientWidth, height: clientHeight } });
 
         // render frame
         currentOutput = await view.render();
