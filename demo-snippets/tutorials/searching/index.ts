8c3b4d3a2dfb8208c2dc25aa095541f6328b62bd
--global <syedaamir974@gmail.com>
2023-04-14 15:03:33 +0500

    test: added snippet for testing search on banenor

--- demo-snippets/tutorials/searching/index.ts
+++ demo-snippets/tutorials/searching/index.ts
@@ -3,6 +3,7 @@ import fluffySearch from "!!./fluffy_search.ts?raw";
 import exactSearch from "!!./exact_search.ts?raw";
 import exactSearchExcludingResult from "!!./exact_search_excluding_result.ts?raw";
 import semanticSearch from "!!./semantic_search.ts?raw";
+import semanticSearch001 from "!!./semantic_search_001.ts?raw";
 
 import { demo } from "../../misc";
 
@@ -12,4 +13,5 @@ export const searching = {
   ...demo("searching", "exactSearch", exactSearch, {}, 'Exact search only checking the property "ifcClass" and the exact value "ifcRoof".'),
   ...demo("searching", "exactSearchExcludingResult", exactSearchExcludingResult, {}, "Same as the Exact search pattern, but with exclude. This will return all objects except the ones found above."),
   ...demo("searching", "semanticSearch", semanticSearch, {}, "Semantic search test."),
+  ...demo("searching", "semanticSearch001", semanticSearch001, {}, "Semantic search test."),
 };
