396b1921ea023dabfb5ccaeba275007011314a30
Aamir Shah <syedaamir974@gmail.com>
2023-07-03 16:46:50 +0500

    Tut(minimap): tut + demo (#17)
    * minimap: initial commit
    
    * image stitch test
    
    * initial commit: zoom via quadtree implementation.
    
    * cleanup
    
    * added minimap helpers from novoweb
    
    * cleanup
    
    * debug
    
    * Working minimap
    
    * Fixes
    
    * another fix
    
    * minor changes
    
    * Merge minimap and quadtree classes
    
    * fix
    
    * Quick fix
    
    * Camera controller fix.
    
    * 1px overlap when drawing on canvas to avoid visible edges
    
    * Tutorial: initial commit.
    
    * Updated with latest from Sigve
    
    * Updated tut
    
    * quickfix
    
    * minor fixes.
    
    * Made small changes, merged latest from @Sigve

---
+++ demo-snippets/tutorials/minimap/index.ts
@@ -0,0 +1,6 @@
+import basicMinimap from "!!./basic_minimap.ts?raw";
+import { demo } from "../../misc";
+
+export const minimap = {
+  ...demo("minimap", "basic_minimap", basicMinimap, { enablePreviewCanvas: true }, "Using tiled images as minimap."),
+};
