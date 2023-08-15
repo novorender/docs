2c8cc6c7b23deab0de53e3548a27bfa783d06967
Aamir Shah <syedaamir974@gmail.com>
2023-08-15 18:25:06 +0500

    chore(cleanup): redundant camera tut

--- demo-snippets/tutorials/cameras/pinhole.ts
+++
@@ -1,8 +0,0 @@
-// should we only use type imports here and pass everything else as params?
-import type * as NovoRender from "@novorender/webgl-api";
-
-export async function init(canvas: HTMLCanvasElement, webglAPI: NovoRender.API) {
-  // provide a camera controller
-  const controller = webglAPI.createCameraController({ kind: "orbit" }, canvas);
-  return { controller } as const;
-}
