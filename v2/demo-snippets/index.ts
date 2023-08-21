97ebb8861a9958459526bd6d48edded883b76539
Tore Lode <16665785+torelode@users.noreply.github.com>
2023-08-21 22:11:41 +0800

    Added code links

--- v2/demo-snippets/index.ts
+++ v2/demo-snippets/index.ts
@@ -1,3 +1,2 @@
 import * as snippets from "./guides";
-
 export { snippets };
---
+++ v2/src/components/CodeLink.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+
+export type TypeString = "class" | "interface" | "type" | "function" | "enum" | "variable";
+
+export const CodeLink = ({ type, name }: { readonly type: TypeString; readonly name: string }) => {
+  let path = "???";
+  let [typeName, ...propNames] = name.split(".");
+  propNames = propNames.map((n) => "#" + (n == "new" ? "new-view" : n.toLowerCase()));
+  const link = typeName + (propNames.length > 0 ? propNames.join("") : "");
+  switch (type) {
+    case "class":
+      path = `Classes/class.${link}`;
+      break;
+    case "interface":
+      path = `Interfaces/interface.${link}`;
+      break;
+    case "type":
+      path = `Type Aliases/type-alias.${link}`;
+      break;
+    case "function":
+      path = `Functions/function.${link}`;
+      break;
+    case "enum":
+      path = `Enumerations/enumeration.${link}`;
+      break;
+    case "variable":
+      path = `Variables/variable.${link}`;
+      break;
+  }
+  const url = useBaseUrl(`docs/web_api/${path}`);
+  return (
+    <a href={url}>
+      <code>{name}</code>
+    </a>
+  );
+};
