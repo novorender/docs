c75a16b65a477f022f980311ccc7a6978daa7633
Tore Lode <16665785+torelode@users.noreply.github.com>
2023-08-21 18:33:33 +0800

    refactored demo and tutorial structure.

--- v2/demo-snippets/hosts/view.ts
+++ v2/demo-snippets/hosts/view.ts
@@ -1,4 +1,4 @@
-import { IDemoContext, IDemoHost, IModule, demo } from "../config";
+import { IDemoContext, IDemoHost, IModule } from "../demo";
 import { RenderStateChanges, View } from "@novorender/api";
 
 /**
@@ -6,10 +6,10 @@ import { RenderStateChanges, View } from "@novorender/api";
  * behind the scenes and is not related to the objective of demo
  * e.g. you can create context or view here that doesn't need to
  * be in the demo
- * @notes host class must implement `IDemoHost<IModule>`
+ * @notes host class must implement `IDemoHost<T>`
  */
-export class RenderStateDemoHost implements IDemoHost<IModule<RenderStateChanges>> {
-  private _module: IModule<RenderStateChanges> | undefined;
+export class ViewDemoHost implements IDemoHost<IModule<RenderStateChanges, View>> {
+  private _module: IModule<RenderStateChanges, View> | undefined;
   private _view: View | undefined;
 
   constructor(readonly context: IDemoContext) {}
@@ -21,32 +21,30 @@ export class RenderStateDemoHost implements IDemoHost<IModule<RenderStateChanges
       imports,
     } = this.context;
     const view = new View(canvas, deviceProfile, imports);
-    const envs = await view.availableEnvironments("https://api.novorender.com/assets/env/index.json");
-    const env = envs[0];
-
-    view.modifyRenderState({ background: { url: env.url, blur: 0.2 } });
 
     this._view = view;
 
     let prev_module = this._module;
 
-    view.animate = () => {
+    view.animate = async () => {
       try {
         if (prev_module !== this._module) {
           prev_module = this._module;
-          const stateChanges = prev_module?.main();
+          const stateChanges = await prev_module?.main(view);
           if (stateChanges) {
             view.modifyRenderState(stateChanges);
           }
         }
-      } catch (error) {}
+      } catch (error) {
+        console.log("error while running module ", error);
+      }
     };
 
     await view.run();
     view.dispose();
   }
 
-  updateModule(module: IModule<RenderStateChanges>): string | undefined {
+  updateModule(module: IModule<RenderStateChanges, View>): string | undefined {
     this._module = module;
     return "test";
   }
--- v2/demo-snippets/tutorials/getting_started/index.ts
+++ v2/demo-snippets/tutorials/getting_started/index.ts
@@ -1,9 +1,8 @@
-/** Here goes  code demo code that you can see in the playground */
+import { demo } from "demo";
+import { RenderStateDemoHost } from "hosts";
+/** Here goes code demo code that you can see in the playground */
 import spheres from "./spheres.ts?raw";
 import basic from "./basic.ts?raw";
-import { IDemoContext, IDemoHost, IModule, demo } from "../../config";
-import { RenderStateDemoHost } from "../hosts";
-import { RenderStateChanges, View } from "@novorender/api";
 
 export const gettingStarted = {
   ...demo("getting_started", "basic_condos", basic, RenderStateDemoHost, {}, "A basic demonstration of the WebGL Web API."),
