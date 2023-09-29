a26b794169fe2fffb16a1453df856f991d8afb2d
Tore Lode <16665785+torelode@users.noreply.github.com>
2023-09-29 10:13:43 +0800

    polishing.

--- demo-snippets/guides/camera_controllers/controllerParams.ts
+++ demo-snippets/guides/camera_controllers/controllerParams.ts
@@ -1,6 +1,7 @@
 import { type OrbitController } from "@novorender/api";
 
 export function main(controller: OrbitController) {
+    // reduce both linear and rotational speed to 10%.
     controller.updateParams({
         linearVelocity: 0.1,
         rotationalVelocity: 0.1,
